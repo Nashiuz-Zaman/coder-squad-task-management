19c0651cb585cc775b0601674f313bc2177b5425
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-03-31 02:26:52 +0600

    Add About section on homepage

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,4 +1,6 @@
 // components
+import InnerContainer from '@/components/containers/InnerContainer/InnerContainer';
+import About from '@/components/page-related/home/About/About';
 import Banner from '@/components/page-related/home/Banner/Banner';
 
 const Home = () => {
@@ -6,6 +8,12 @@ const Home = () => {
       <>
          <section className='mt-sectionGapSm 2md:mt-sectionGapMd lg:mt-24 xl:mt-[7.5rem] mb-sectionGapMd md:mb-sectionGapLg'>
             <Banner />
+         </section>
+
+         <section id='learn-more' className='mb-sectionGapMd md:mb-sectionGapLg'>
+            <InnerContainer>
+               <About />
+            </InnerContainer>
          </section>
       </>
    );

67b0cf1bb4d225851e3b48e2e14f7ce10b1274a7
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-03-30 18:40:07 +0600

    Create InnerContainer component and Header component, add Brandlogo component in Header and place the whole Header in mainLayout

---
+++ src/components/shared/Header/Header.jsx
@@ -0,0 +1,30 @@
+'use client';
+
+// react
+import PropTypes from 'prop-types';
+
+// components
+import BrandLogo from '@/components/shared/BrandLogo/BrandLogo';
+import InnerContainer from '@/components/containers/InnerContainer/InnerContainer';
+
+const Header = ({ modifyClasses = '' }) => {
+  return (
+    <header className={`pt-5 lg:pt-6 ${modifyClasses}`}>
+      <InnerContainer>
+        <div className='grid grid-cols-[1fr_3fr_1fr] 2md:grid-cols-2 items-center'>
+          {/* div for design purpose */}
+          <div className='2md:hidden'>&nbsp;</div>
+
+          {/* website logo */}
+          <BrandLogo modifyClasses='justify-self-center 2md:justify-self-start' />
+        </div>
+      </InnerContainer>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  modifyClasses: PropTypes.string,
+};
+
+export default Header;

3e1decbfbd6d66e1af9a23055669d7debf044f20
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-04-02 23:43:00 +0600

    Add btns for editing, switching status, viewing details of the task

--- src/components/buttons/index.jsx
+++ src/components/buttons/index.jsx
@@ -6,7 +6,26 @@ import CloseBtn from '@/components/buttons/CloseBtn/CloseBtn';
 import LinkBtn from '@/components/buttons/LinkBtn/LinkBtn';
 import MobileMenuBtn from '@/components/buttons/MobileMenuBtn/MobileMenuBtn';
 import DeleteBtn from '@/components/buttons/DeleteBtn/DeleteBtn';
-import AddBtn from './AddBtn/AddBtn';
+import AddBtn from '@/components/buttons/AddBtn/AddBtn';
+import MoveToCompletedBtn from '@/components/buttons/MoveToCompletedBtn/MoveToCompletedBtn';
+import MoveToTodoBtn from '@/components/buttons/MoveToTodoBtn/MoveToTodoBtn';
+import MoveToOngoingBtn from '@/components/buttons/MoveToOngoingBtn/MoveToOngoingBtn';
+import EditBtn from '@/components/buttons/EditBtn/EditBtn';
+import ViewDetailsBtn from '@/components/buttons/ViewDetailsBtn/ViewDetailsBtn';
+import DotsMenuBtn from '@/components/buttons/DotsMenuBtn/DotsMenuBtn';
 
 // export components
-export { ButtonBtn, CloseBtn, LinkBtn, MobileMenuBtn, DeleteBtn, AddBtn };
+export {
+   ButtonBtn,
+   CloseBtn,
+   LinkBtn,
+   MobileMenuBtn,
+   DeleteBtn,
+   AddBtn,
+   MoveToCompletedBtn,
+   MoveToTodoBtn,
+   MoveToOngoingBtn,
+   EditBtn,
+   ViewDetailsBtn,
+   DotsMenuBtn,
+};

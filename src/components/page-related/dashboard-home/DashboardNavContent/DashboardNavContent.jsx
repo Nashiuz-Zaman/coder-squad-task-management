5983ce8b7b4cdacad36c1f122dab4ae10ee58128
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-04-02 17:15:19 +0600

    Add Task Fetching component, UserProfile and dashboard layout related components to project

---
+++ src/components/page-related/dashboard-home/DashboardNavContent/DashboardNavContent.jsx
@@ -0,0 +1,59 @@
+'use client';
+
+// react
+import PropTypes from 'prop-types';
+
+// icons
+import { Icon } from '@iconify/react';
+
+// hook
+import { useDashboardMobileNav } from '@/hooks';
+
+// components
+import DashboardNavHeading from '../DashboardNavHeading/DashboardNavHeading';
+import DashboardNavLink from '../DashboardNavLink/DashboardNavLink';
+
+// redux
+import { useSelector } from 'react-redux';
+
+const DashboardNavContent = ({ modifyClasses = '' }) => {  
+   const { profileData } = useSelector(store => store.auth);
+   const { closeDashboardMobileNav } = useDashboardMobileNav();
+
+   const primaryOptions = [
+      {
+         text: 'Manage Tasks',
+         icon: <Icon icon='fluent:tasks-app-20-filled' />,
+         url: `/manage-tasks?id=${profileData?._id}`,
+      },
+   ];
+
+   return (
+      <div className={`space-y-3 md:space-y-6 ${modifyClasses}`}>
+         {/* dashboard pages */}
+         <div className='pb-6 border-b border-neutral-200'>
+            <DashboardNavHeading text='Menu' modifyClasses='mb-2 md:mb-6' />
+
+            {/* the navigation menu */}
+            <ul className='space-y-3'>
+               {primaryOptions.map((option, i) => {
+                  return (
+                     <li key={i} className='ml-4'>
+                        <DashboardNavLink
+                           onClickFunction={() => closeDashboardMobileNav()}
+                           linkData={option}
+                        />
+                     </li>
+                  );
+               })}
+            </ul>
+         </div>
+      </div>
+   );
+};
+
+DashboardNavContent.propTypes = {
+   modifyClasses: PropTypes.string,
+};
+
+export default DashboardNavContent;

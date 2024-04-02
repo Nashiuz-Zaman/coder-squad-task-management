890e6a346c34a74f2eb61c8822891acf93d23049
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-04-02 17:10:55 +0600

    Add dashboard and task redux slices, update central store accordingly

--- src/lib/redux/store.js
+++ src/lib/redux/store.js
@@ -8,13 +8,17 @@ import backdropReducer from '@/lib/redux/features/backdrop/backdropSlice';
 import mediaQueryReducer from '@/lib/redux/features/mediaQuery/mediaQuerySlice';
 import mobileNavReducer from '@/lib/redux/features/mobileNav/mobileNavSlice';
 import authReducer from '@/lib/redux/features/auth/authSlice';
+import dashboardReducer from '@/lib/redux/features/dashboard/dashboardSlice';
+import taskReducer from '@/lib/redux/features/task/taskSlice';
 
 // store
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    backdrop: backdropReducer,
-    mediaQuery: mediaQueryReducer,
-    mobileNav: mobileNavReducer,
-  },
+   reducer: {
+      auth: authReducer,
+      backdrop: backdropReducer,
+      mediaQuery: mediaQueryReducer,
+      mobileNav: mobileNavReducer,
+      dashboard: dashboardReducer,
+      task: taskReducer
+   },
 });

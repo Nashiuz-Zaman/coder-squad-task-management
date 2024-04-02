d57293e430f8a3f356098f9e7f75f0a47bcdb37a
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-04-02 18:47:07 +0600

    Add TaskCreate and TaskEdit forms to project

---
+++ src/components/forms/TaskCreateForm/TaskCreateForm.jsx
@@ -0,0 +1,110 @@
+'use client';
+
+// shared components
+import { ButtonBtn, CloseBtn } from '@/components/buttons';
+import { SelectField, InputField2, TextareaField } from '@/components/shared';
+
+// custom hooks
+import {
+   useTaskDatabaseMethods,
+   useEscapeClose,
+   useFormVisiblity,
+   useClickOutside,
+} from '@/hooks';
+
+// redux
+import { useSelector } from 'react-redux';
+
+// data
+import { priorityOptions } from '@/uiData/formsUiData';
+
+const TaskCreateForm = () => {
+   const { profileData } = useSelector(store => store.auth);
+   const { taskCreateFormOpen } = useSelector(store => store.form);
+   const { createTask } = useTaskDatabaseMethods();
+   const { closeTaskCreateForm } = useFormVisiblity();
+
+   const handleClickOutside = e => {
+      if (!e.target.closest('.task-create-form-focus')) {
+         closeTaskCreateForm();
+      }
+   };
+
+   // add support clicking out side and escape key press
+   useEscapeClose(closeTaskCreateForm);
+   useClickOutside(taskCreateFormOpen, handleClickOutside);
+
+   const handleCreateTask = e => {
+      e.preventDefault();
+
+      // take all the necessary values
+      const form = e.target;
+      const title = form.title.value;
+      const description = form.description.value;
+      const deadline = new Date(form.deadline.value).toISOString();
+      const priorityLevel = parseInt(form.priority.value);
+      const date = new Date().toISOString();
+
+      // Task data
+      const taskData = {
+         title,
+         description,
+         statusLevel: 0,
+         deadline,
+         priorityLevel,
+         lastUpdated: date,
+         email: profileData.email,
+      };
+
+      createTask(taskData);
+      closeTaskCreateForm();
+      form.reset();
+   };
+
+   return (
+      <div
+         className={`${
+            taskCreateFormOpen ? 'block' : 'hidden'
+         } translate-x-4 md:translate-x-8 shadow-md w-[16rem] xs:w-[19rem] absolute top-0 translate-y-[3.2rem] md:translate-y-[3.7rem] left-0 p-4 bg-white border border-neutral-300 z-40 rounded-xl task-create-form-focus`}
+      >
+         <CloseBtn
+            onClickFunction={closeTaskCreateForm}
+            modifyClasses='!text-xl'
+         />
+
+         {/* form starts here */}
+         <form onSubmit={handleCreateTask} className='block space-y-3'>
+            {/* title */}
+            <InputField2
+               label='Title'
+               maxLength={50}
+               name='title'
+               placeholder='Task Title'
+            />
+
+            {/* description */}
+            <TextareaField
+               label='Description'
+               name='description'
+               placeholder='Task Description'
+            />
+
+            {/* deadline */}
+            <InputField2 label='Deadline' type='date' name='deadline' />
+
+            {/* priority */}
+            <SelectField
+               label='Priority'
+               name='priority'
+               options={priorityOptions}
+               defaultValueData={0}
+            />
+
+            {/* submit button */}
+            <ButtonBtn text='Add Task' modifyClasses='!ml-auto !mt-5' />
+         </form>
+      </div>
+   );
+};
+
+export default TaskCreateForm;
---
+++ src/components/forms/TaskEditForm/TaskEditForm.jsx
@@ -0,0 +1,116 @@
+'use client';
+
+// shared components
+import { ButtonBtn, CloseBtn } from '@/components/buttons';
+import { InputField2, TextareaField, SelectField } from '@/components/shared';
+
+// custom hooks
+import {
+   useTaskDatabaseMethods,
+   useEscapeClose,
+   useFormVisiblity,
+   useRedux
+} from '@/hooks';
+
+// redux
+import { setTaskToEdit } from '@/lib/redux/features/task/taskSlice';
+
+// data
+import { priorityOptions } from '@/uiData/formsUiData';
+
+// utils
+import { getDateInYYYYMMDD } from '@/utils/dateTimeMethods';
+
+const TaskEditForm = () => {
+   const { dispatch, useSelector } = useRedux();
+   const { taskEditFormOpen } = useSelector(store => store.form);
+   const { taskToEdit, totalTasks } = useSelector(store => store.task);
+   const { closeTaskEditForm } = useFormVisiblity();
+   const { editTask } = useTaskDatabaseMethods();
+
+   // close form and reset the taskToEdit state
+   const handleCloseForm = () => {
+      closeTaskEditForm();
+      dispatch(setTaskToEdit(null));
+   };
+
+   // add support clicking outside and escape key press
+   useEscapeClose(handleCloseForm);
+
+   // all form values come from their inputs but date comes from the state
+   const handleEditTask = e => {
+      e.preventDefault();
+
+      // take all the necessary values
+      const form = e.target;
+      const title = form.title.value.trim();
+      const description = form.description.value.trim();
+      const deadline = form.deadline.value.trim();
+      const priorityLevel = parseInt(form.priority.value.trim());
+
+      // Task data summarized
+      const editedTaskData = {
+         title,
+         description,
+         deadline,
+         priorityLevel,
+      };
+
+      editTask(taskToEdit._id, editedTaskData, totalTasks);
+      handleCloseForm();
+      form.reset();
+   };
+
+   return (
+      <div
+         className={`${
+            taskEditFormOpen ? 'opacity-100 visible' : 'opacity-0 collapse'
+         } transition-all !duration-default shadow-small w-[90%] 2xs:w-[20rem] md:w-[30rem] border border-neutral-300 fixed top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 p-4 bg-white z-40 rounded-xl task-edit-form-focus`}
+      >
+         <CloseBtn onClickFunction={handleCloseForm} modifyClasses='!text-xl' />
+
+         {/* form starts here */}
+         <form onSubmit={handleEditTask} className='block space-y-3'>
+            {/* title */}
+            <InputField2
+               defaultValueData={taskToEdit?.title}
+               label='Title'
+               name='title'
+               maxLength={50}
+               placeholder='Task Title'
+            />
+
+            {/* description */}
+            <TextareaField
+               defaultValueData={taskToEdit?.description}
+               label='Description'
+               name='description'
+               placeholder='Task Description'
+            />
+
+            {/* deadline */}
+            <InputField2
+               defaultValueData={
+                  taskToEdit && getDateInYYYYMMDD(taskToEdit.deadline)
+               }
+               type='date'
+               label='Deadline'
+               name='deadline'
+            />
+
+            {/* priority */}
+            <SelectField
+               label='Priority'
+               name='priority'
+               options={priorityOptions}
+               defaultValueData={taskToEdit?.priorityLevel}
+            />
+
+            {/* submit button */}
+            <ButtonBtn text='Save' modifyClasses='!ml-auto !mt-5' />
+         </form>
+      </div>
+   );
+};
+
+export default TaskEditForm;
